e0ea800b003f86d0bfcf3f80820902a61ce9b12e
Ben Dobbs <bdobbs@archwaytrust.co.uk>
2021-01-06 11:41:35 +0000

    Added attendance date to academy security.

---
+++ G4SApiSync.Data/Migrations/20210106114059_Migration9.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace G4SApiSync.Data.Migrations
+{
+    public partial class Migration9 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AttendanceFrom",
+                schema: "sec",
+                table: "AcademySecurity",
+                type: "Date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AttendanceTo",
+                schema: "sec",
+                table: "AcademySecurity",
+                type: "Date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "GetAttendance",
+                schema: "sec",
+                table: "AcademySecurity",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AttendanceFrom",
+                schema: "sec",
+                table: "AcademySecurity");
+
+            migrationBuilder.DropColumn(
+                name: "AttendanceTo",
+                schema: "sec",
+                table: "AcademySecurity");
+
+            migrationBuilder.DropColumn(
+                name: "GetAttendance",
+                schema: "sec",
+                table: "AcademySecurity");
+        }
+    }
+}
