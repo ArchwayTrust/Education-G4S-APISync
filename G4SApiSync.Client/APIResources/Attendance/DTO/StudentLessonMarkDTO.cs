b29ceb86660c597ae4786be94b017595daa3d442
Ben Dobbs <bdobbs@archwaytrust.co.uk>
2021-01-06 12:58:05 +0000

    Constructed lesson mark DTO and duplicate a GET routine.

---
+++ G4SApiSync.Client/APIResources/Attendance/DTO/StudentLessonMarkDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace G4SApiSync.Client.DTOs
+{
+        [JsonObject]
+        public class StudentLessonMarkDTO
+        {
+            [JsonProperty("date")]
+            public DateTime Date { get; set; }
+
+            [JsonProperty("student_id")]
+            public int G4SStudentId{ get; set; }
+            
+            [JsonProperty("class_id")]
+            public int G4SClassId { get; set; }
+
+            [JsonProperty("lesson_mark_id")]
+            public int G4SMarkId { get; set; }
+
+            [JsonProperty("lesson_alias_id")]
+            public int G4SAliasId { get; set; }
+
+            [JsonProperty("lesson_minutes_late")]
+            public int? LessonMinutesLate { get; set; }
+
+            [JsonProperty("lesson_notes")]
+            public string LessonNotes { get; set; }
+    }
+}
