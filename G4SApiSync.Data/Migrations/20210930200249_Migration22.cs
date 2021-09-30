1a20c3a04f2468cafe14d4db902305add3fd3ead
Benjamin Dobbs <ben@dobbs.me.uk>
2021-09-30 21:04:11 +0100

    Changed event date to date time.

---
+++ G4SApiSync.Data/Migrations/20210930200249_Migration22.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace G4SApiSync.Data.Migrations
+{
+    public partial class Migration22 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EventDate",
+                schema: "g4s",
+                table: "BehEvents",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "Date");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EventDate",
+                schema: "g4s",
+                table: "BehEvents",
+                type: "Date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
