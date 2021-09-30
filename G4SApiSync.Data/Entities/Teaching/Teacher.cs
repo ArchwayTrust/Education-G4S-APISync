515e62f37802395ddb215b705cf40317983e08dd
Benjamin Dobbs <ben@dobbs.me.uk>
2021-09-30 10:29:43 +0100

    Added teacher entitity.

---
+++ G4SApiSync.Data/Entities/Teaching/Teacher.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace G4SApiSync.Data.Entities
+{
+    public class Teacher
+    {
+        [MaxLength(100)]
+        [Key]
+        public string TeacherId { get; set; } //AcademyCode + DataSet + "-" + G4STeacherId
+
+        public int G4STeacherId { get; set; }
+
+        [MaxLength(4)]
+        public string DataSet { get; set; }
+
+        [MaxLength(10)]
+        public string Academy { get; set; }
+
+        [MaxLength(10)]
+        public string Title { get; set; }
+
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [MaxLength(100)]
+        public string PreferredFirstName { get; set; }
+
+        [MaxLength(100)]
+        public string PreferredLastName { get; set; }
+
+        [MaxLength(10)]
+        public string Initials { get; set; }
+
+        [MaxLength(10)]
+        public string Code { get; set; }
+
+    }
+}
---
+++ G4SApiSync.Data/Migrations/20210930092342_Migration13.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace G4SApiSync.Data.Migrations
+{
+    public partial class Migration13 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                schema: "g4s",
+                columns: table => new
+                {
+                    TeacherId = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    G4STeacherId = table.Column<int>(type: "int", nullable: false),
+                    DataSet = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: true),
+                    Academy = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    PreferredFirstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    PreferredLastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Initials = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    Code = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.TeacherId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Teachers",
+                schema: "g4s");
+        }
+    }
+}
