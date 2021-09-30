a191b49e54f2d5f9770bd55e5f1e834276d5dd2c
Benjamin Dobbs <ben@dobbs.me.uk>
2021-09-30 10:35:36 +0100

    Added TeacherDTO

---
+++ G4SApiSync.Client/APIResources/Teaching/DTO/TeacherDTO.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+namespace G4SApiSync.Client.DTOs
+{
+    public class TeacherDTO
+    {
+        [JsonProperty("id")]
+        public int G4STeacherId { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+
+        [JsonProperty("preferred_first_name")]
+        public string PreferredFirstName { get; set; }
+
+        [JsonProperty("preferred_last_name")]
+        public string PreferredLastName { get; set; }
+
+        [JsonProperty("initials")]
+        public string Initials { get; set; }
+
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+    }
+}
