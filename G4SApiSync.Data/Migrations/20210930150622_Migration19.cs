91ba0692b5b2e29dc5f4b3e39c9f618c32c1dfd3
Benjamin Dobbs <ben@dobbs.me.uk>
2021-09-30 16:23:44 +0100

    Removed relationships between Event Types and Classifications.

---
+++ G4SApiSync.Data/Migrations/20210930150622_Migration19.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace G4SApiSync.Data.Migrations
+{
+    public partial class Migration19 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BehaviourFrom",
+                schema: "sec",
+                table: "AcademySecurity",
+                type: "Date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BehaviourTo",
+                schema: "sec",
+                table: "AcademySecurity",
+                type: "Date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "GetBehaviour",
+                schema: "sec",
+                table: "AcademySecurity",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BehaviourFrom",
+                schema: "sec",
+                table: "AcademySecurity");
+
+            migrationBuilder.DropColumn(
+                name: "BehaviourTo",
+                schema: "sec",
+                table: "AcademySecurity");
+
+            migrationBuilder.DropColumn(
+                name: "GetBehaviour",
+                schema: "sec",
+                table: "AcademySecurity");
+        }
+    }
+}
---
+++ G4SApiSync.Data/Migrations/20210930152249_Migration20.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace G4SApiSync.Data.Migrations
+{
+    public partial class Migration20 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BehEvents_BehEventTypes_BehEventTypeId",
+                schema: "g4s",
+                table: "BehEvents");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_BehEventTypes_BehClassifications_BehClassificationId",
+                schema: "g4s",
+                table: "BehEventTypes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BehEvents_BehEventTypes_BehEventTypeId",
+                schema: "g4s",
+                table: "BehEvents",
+                column: "BehEventTypeId",
+                principalSchema: "g4s",
+                principalTable: "BehEventTypes",
+                principalColumn: "BehEventTypeId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BehEventTypes_BehClassifications_BehClassificationId",
+                schema: "g4s",
+                table: "BehEventTypes",
+                column: "BehClassificationId",
+                principalSchema: "g4s",
+                principalTable: "BehClassifications",
+                principalColumn: "BehClassificationId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BehEvents_BehEventTypes_BehEventTypeId",
+                schema: "g4s",
+                table: "BehEvents");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_BehEventTypes_BehClassifications_BehClassificationId",
+                schema: "g4s",
+                table: "BehEventTypes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BehEvents_BehEventTypes_BehEventTypeId",
+                schema: "g4s",
+                table: "BehEvents",
+                column: "BehEventTypeId",
+                principalSchema: "g4s",
+                principalTable: "BehEventTypes",
+                principalColumn: "BehEventTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BehEventTypes_BehClassifications_BehClassificationId",
+                schema: "g4s",
+                table: "BehEventTypes",
+                column: "BehClassificationId",
+                principalSchema: "g4s",
+                principalTable: "BehClassifications",
+                principalColumn: "BehClassificationId");
+        }
+    }
+}
