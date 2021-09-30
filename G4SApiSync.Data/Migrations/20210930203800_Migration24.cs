eaa18fd1a288f8ad91f645c49e6d127982bac813
Benjamin Dobbs <ben@dobbs.me.uk>
2021-09-30 21:39:40 +0100

    Set back to datetime2

---
+++ G4SApiSync.Data/Migrations/20210930203800_Migration24.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace G4SApiSync.Data.Migrations
+{
+    public partial class Migration24 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedTimeStamp",
+                schema: "g4s",
+                table: "BehEvents",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "Datetime");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EventDate",
+                schema: "g4s",
+                table: "BehEvents",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "Datetime");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedTimeStamp",
+                schema: "g4s",
+                table: "BehEvents",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "Datetime");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedTimeStamp",
+                schema: "g4s",
+                table: "BehEvents",
+                type: "Datetime",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EventDate",
+                schema: "g4s",
+                table: "BehEvents",
+                type: "Datetime",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedTimeStamp",
+                schema: "g4s",
+                table: "BehEvents",
+                type: "Datetime",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
