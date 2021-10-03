edd1d3eae875cdd4c82ad752cddf0864c0f75dc1
Ben Dobbs <bdobbs@archwaytrust.co.uk>
2021-10-03 10:38:38 +0100

    Improved scalar udf code and updated student view.

--- G4SApiSync/SQL Scripts/CreateScalarUDFsForAttributes.sql
+++ G4SApiSync/SQL Scripts/CreateScalarUDFsForAttributes.sql
@@ -5,6 +5,7 @@ CREATE OR ALTER FUNCTION g4s.udfAttributeValueAcademicYear(
 	@StudentId NVARCHAR(100),
 	@Academy NVARCHAR(10),
 	@DataSet NVARCHAR(4),
+	@AcademicYear NVARCHAR(4),
 	@AttributeName NVARCHAR(500)
 )
 
@@ -23,8 +24,9 @@ SET @ReturnValue =
 		t.DataSet = @DataSet
 		AND t.Academy = @Academy
 		AND t.AttributeName = @AttributeName
-		AND v.AcademicYear = @DataSet
-		AND v.StudentId = @StudentId)
+		AND v.AcademicYear = @AcademicYear
+		AND v.StudentId = @StudentId
+		AND v.[Date] IS NULL)
 
 RETURN @ReturnValue
 END;
@@ -53,13 +55,15 @@ SET @ReturnValue =
 		t.DataSet = @DataSet
 		AND t.Academy = @Academy
 		AND t.AttributeName = @AttributeName
-		AND v.StudentId = @StudentId)
+		AND v.StudentId = @StudentId
+		AND v.[Date] IS NULL
+		AND v.[AcademicYear] IS NULL)
 
 RETURN @ReturnValue
 END;
 GO
 
-CREATE OR ALTER FUNCTION g4s.udfAttributeValueLatestDate(
+CREATE OR ALTER FUNCTION g4s.udfAttributeValueLatest(
 	@StudentId NVARCHAR(100),
 	@Academy NVARCHAR(10),
 	@DataSet NVARCHAR(4),
@@ -82,7 +86,36 @@ SET @ReturnValue =
 		AND t.Academy = @Academy
 		AND t.AttributeName = @AttributeName
 		AND v.StudentId = @StudentId
-	ORDER BY v.[Date] DESC)
+	ORDER BY v.[Date] DESC, v.AcademicYear DESC)
+
+RETURN @ReturnValue
+END;
+GO
+
+CREATE OR ALTER FUNCTION g4s.udfAttributeValueEarliest(
+	@StudentId NVARCHAR(100),
+	@Academy NVARCHAR(10),
+	@DataSet NVARCHAR(4),
+	@AttributeName NVARCHAR(500)
+)
+
+RETURNS NVARCHAR(1000)
+AS
+
+BEGIN
+DECLARE @ReturnValue AS NVARCHAR(1000)
+SET @ReturnValue = 
+	(SELECT TOP 1
+		v.[Value]
+	FROM 
+		g4s.AttributeTypes AS t
+		LEFT JOIN g4s.AttributeValues AS v ON v.AttributeTypeId = t.AttributeTypeId
+	WHERE 
+		t.DataSet = @DataSet
+		AND t.Academy = @Academy
+		AND t.AttributeName = @AttributeName
+		AND v.StudentId = @StudentId
+	ORDER BY v.[Date] ASC, v.AcademicYear ASC)
 
 RETURN @ReturnValue
 END;
